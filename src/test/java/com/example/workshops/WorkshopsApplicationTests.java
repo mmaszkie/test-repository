49e8d109a85a94b8daebf9eca173455ea4f38622
patryk-cierniak <killua12292@gmail.com>
2024-04-04 10:14:33 +0200

    test

--- src/test/java/com/example/workshops/WorkshopsApplicationTests.java
+++ src/test/java/com/example/workshops/WorkshopsApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.workshops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WorkshopsApplicationTests {
 
-
 	void contextLoads() {
+	}
+	@Test
+	void failedTest(){
+		Assertions.assertEquals(1,2);
 	}
 
 }

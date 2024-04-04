0a793d4c2d8cba9cc91e571f473ff250d778268d
Frodzislaw <frodzislaw2001@onet.eu>
2024-04-04 10:15:48 +0200

    Ten ci dodanyefef4r4r4r

--- src/test/java/com/example/workshops/WorkshopsApplicationTests.java
+++ src/test/java/com/example/workshops/WorkshopsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.workshops;
 
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class WorkshopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals(1, 2);
 	}
 
 }

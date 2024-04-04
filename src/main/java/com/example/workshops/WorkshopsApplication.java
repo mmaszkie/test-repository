46989a5f251fdc1772dbb8423585d9e35e51fb76
Frodzislaw <frodzislaw2001@onet.eu>
2024-04-04 10:12:35 +0200

    Ten ci dodanyefef

--- src/main/java/com/example/workshops/WorkshopsApplication.java
+++ src/main/java/com/example/workshops/WorkshopsApplication.java
@@ -6,5 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WorkshopsApplication {
 
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopsApplication.class, args);
+	}
 
 }

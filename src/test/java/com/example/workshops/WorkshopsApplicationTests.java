f054a8f6103d66f2d45ba5a5a6654702b00bc068
dkwasniewski9 <pingwin296@gmail.com>
2024-04-04 10:17:39 +0200

    fail test

--- src/test/java/com/example/workshops/WorkshopsApplicationTests.java
+++ src/test/java/com/example/workshops/WorkshopsApplicationTests.java
@@ -2,12 +2,14 @@ package com.example.workshops;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class WorkshopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assert.isTrue(false, "false");
 	}
 
 }
